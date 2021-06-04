51c2b9cae423b2a21a1a8d30fb261d397eabf51b
canerdk <canerdk1995@hotmail.com>
2021-06-04 03:10:05 +0300

    added datetime for document and wwwroot

--- DAL/Entities/Document.cs
+++ DAL/Entities/Document.cs
@@ -10,6 +10,7 @@ namespace EBookWebApi.DAL.Entities
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public DateTime AddDate { get; set; }
         public string Url { get; set; }
         public Guid BookId { get; set; }
     }
